6ad996a6cb61e9174526021762a6242949bbc701
ilyaleonovStudy <ilyaleonov_study@mail.ru>
2025-07-15 20:30:32 +0300

    test

--- main.js
+++ main.js
@@ -23,7 +23,7 @@
 });
 
 
-const imageFolder = 'assets/example-img/';
+const imageFolder = './assets/example-img/';
 
 const imgExample = [
     'example-1.jpg',
@@ -49,7 +49,7 @@ const container = document.querySelector('.example__img-container');
   imgExample.forEach(imgName => {
     const slideDiv = document.createElement('div');
     slideDiv.classList.add('swiper-slide', 'example-slide');
-    slideDiv.style.background = `url('/${imageFolder + imgName}') no-repeat center center`;
+    slideDiv.style.background = `url('${imageFolder + imgName}') no-repeat center center`;
     slideDiv.style.backgroundSize = 'cover';
 
     wrapperDiv.appendChild(slideDiv);
@@ -58,7 +58,7 @@ const container = document.querySelector('.example__img-container');
   swiperDiv.appendChild(wrapperDiv);
   container.appendChild(swiperDiv);
 
-const imgPath = 'assets/certificate-img/';
+const imgPath = './assets/certificate-img/';
 
   const imgCertificate = [
     'certificate-1.jpg',
@@ -103,7 +103,7 @@ const imgPath = 'assets/certificate-img/';
   imgCertificate.forEach(fileName => {
     const slide = document.createElement('div');
     slide.className = 'swiper-slide advantages-slide';
-    slide.style.background = `url('/${imgPath + fileName}') no-repeat center center`;
+    slide.style.background = `url('${imgPath + fileName}') no-repeat center center`;
     slide.style.backgroundSize = 'cover';
     slide.style.height = '200px';
 
