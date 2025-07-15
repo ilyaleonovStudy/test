1f6112db2dfbbf4577075befbe7008f443e8f08c
ilyaleonovStudy <ilyaleonov_study@mail.ru>
2025-07-15 20:09:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
   document.addEventListener("DOMContentLoaded", function () {
     const message = "Здравствуйте Людмила! Подскажите, пожалуйста, на какое время можно к Вам записаться?";
-	const number = "79500462820";
+	const number = "79085871055";
 	const vkProfile = "https://vk.com/liudmilasv_nails";
 
     const encodedMessage = encodeURIComponent(message);
@@ -17,7 +17,7 @@
     });
 
     document.querySelectorAll('[data-third-btn="thirdBtn"]').forEach(button => {
-      button.setAttribute("href", `tel:${number}`);
+      button.setAttribute("href", `tel:+${number}`);
       button.setAttribute("target", "_blank");
     });
 });
@@ -25,12 +25,17 @@
 
 const imageFolder = 'assets/example-img/';
 
-const imageNames = [
-    'adv1.jpg',
-    'adv2.jpg',
-    'adv3.jpg',
-	'adv4.jpg',
-	'adv2.jpg',
+const imgExample = [
+    'example-1.jpg',
+    'example-2.jpg',
+    'example-3.jpg',
+    'example-4.jpg',
+    'example-5.jpg',
+    'example-6.jpg',
+    'example-7.jpg',
+    'example-8.jpg',
+    'example-9.jpg',
+    'example-10.jpg',
   ];
 
 const container = document.querySelector('.example__img-container');
@@ -41,7 +46,7 @@ const container = document.querySelector('.example__img-container');
   const wrapperDiv = document.createElement('div');
   wrapperDiv.classList.add('swiper-wrapper');
 
-  imageNames.forEach(imgName => {
+  imgExample.forEach(imgName => {
     const slideDiv = document.createElement('div');
     slideDiv.classList.add('swiper-slide', 'example-slide');
     slideDiv.style.background = `url('/${imageFolder + imgName}') no-repeat center center`;
@@ -55,12 +60,36 @@ const container = document.querySelector('.example__img-container');
 
 const imgPath = 'assets/certificate-img/';
 
-  const imgFiles = [
-    'adv1.jpg',
-    'adv2.jpg',
-    'adv3.jpg',
-    'adv4.jpg',
-    'adv1.jpg'
+  const imgCertificate = [
+    'certificate-1.jpg',
+    'certificate-2.jpg',
+    'certificate-3.jpg',
+    'certificate-4.jpg',
+    'certificate-5.jpg',
+    'certificate-6.jpg',
+    'certificate-7.jpg',
+    'certificate-8.jpg',
+    'certificate-9.jpg',
+    'certificate-10.jpg',
+    'certificate-11.jpg',
+    'certificate-12.jpg',
+    'certificate-13.jpg',
+    'certificate-14.jpg',
+    'certificate-15.jpg',
+    'certificate-16.jpg',
+    'certificate-17.jpg',
+    'certificate-18.jpg',
+    'certificate-19.jpg',
+    'certificate-20.jpg',
+    'certificate-21.jpg',
+    'certificate-22.jpg',
+    'certificate-23.jpg',
+    'certificate-24.jpg',
+    'certificate-25.jpg',
+    'certificate-26.jpg',
+    'certificate-27.jpg',
+    'certificate-28.jpg',
+
   ];
 
   const targetContainer = document.querySelector('.about__left-advantages');
@@ -71,7 +100,7 @@ const imgPath = 'assets/certificate-img/';
   const slidesContainer = document.createElement('div');
   slidesContainer.className = 'swiper-wrapper advantages-wrapper';
 
-  imgFiles.forEach(fileName => {
+  imgCertificate.forEach(fileName => {
     const slide = document.createElement('div');
     slide.className = 'swiper-slide advantages-slide';
     slide.style.background = `url('${imgPath + fileName}') no-repeat center center`;
@@ -112,7 +141,11 @@ const imgPath = 'assets/certificate-img/';
 	1330: {
 	slidesPerView: 4 
 	}
-}
+},
+pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
 });
 
 let swiperExample = new Swiper(".example-slider", {
@@ -134,3 +167,9 @@ let swiperExample = new Swiper(".example-slider", {
 	}
 }
 });
+
+AOS.init({
+    duration: 1000,
+});
+
+
